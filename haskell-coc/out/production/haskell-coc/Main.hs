cd5364feb0e43bc5290c4c6c41a97b419efe4869
jonathan <jonathantbrouwer@gmail.com>
2022-11-09 12:23:35 +0100

    Work on haskell coc

--- haskell-coc/out/production/haskell-coc/Main.hs
+++ haskell-coc/out/production/haskell-coc/Main.hs
@@ -39,6 +39,58 @@ brh_ (s, FnConstruct a b) [] = Just (s, FnConstruct a b)
 brh_ (s, FnConstruct _ b) (t : tt) = brh_ (sPutSubst s t, b) tt
 brh_ (s, FnDestruct f a) tt = brh_ (s, f) ((s, a) : tt)
 
+br :: SExpr -> Expr
+br se = case brh se of
+  (_, Type) -> Type
+  (_, Var i) -> Var i
+  (s, FnType a b) -> FnType (br (s, a)) (br (s, b))
+  (s, FnConstruct a b) -> FnConstruct (br (s, a)) (br (s, b))
+  (s, FnDestruct a b) -> FnDestruct (br (s, a)) (br (s, b))
+
+beq :: SExpr -> SExpr -> Either String ()
+beq e1 e2 = case (brh e1, brh e2) of
+  ((_, Type), (_, Type)) -> pure ()
+  ((_, Var i), (_, Var j)) -> if i == j then pure () else Left $ "Expected variable " ++ show j ++ ", but got variable " ++ show i
+  ((s1, FnType a1 b1), (s2, FnType a2 b2)) -> do
+     beq (s1, a1) (s2, a2)
+     beq (sPutType s1 a1, b1) (sPutType s2 a2, b2)
+  ((s1, FnConstruct a1 b1), (s2, FnConstruct a2 b2)) -> do
+     beq (s1, a1) (s2, a2)
+     beq (s1, b1) (s2, b2)
+  ((s1, FnDestruct a1 b1), (s2, FnDestruct a2 b2)) -> do
+    beq (s1, a1) (s2, a2)
+    beq (s1, b1) (s2, b2)
+  _ -> Left $ "Expected " ++ show e2 ++ ", but got " ++ show e1
+
+tc :: SExpr -> Either String Expr
+tc (_, Type) = pure Type
+tc (s, Var i) = case s !! i of
+  NType t -> pure t
+  NSubst es -> tc es
+tc (s, Let v b) = do
+  tc (s, v)
+  tc (sPutSubst s (s, v), b)
+tc (s, FnType a b) = do
+  at <- tc (s, a)
+  beq ([], at) ([], Type)
+  let at' = br (s, a)
+  bt <- tc (sPutType s at', b)
+  beq ([], bt) ([], Type)
+  pure Type
+tc (s, FnConstruct a b) = do
+  at <- tc (s, a)
+  beq ([], at) ([], Type)
+  let at' = br (s, a)
+  bt <- tc (sPutType s at', b)
+  pure $ FnType at' bt
+tc (s, FnDestruct f a) = do
+  ft <- tc (s, f)
+  at <- tc (s, a)
+  (sf, dt, db) <- case brh ([], ft) of
+    (sf, FnType dt db) -> pure (sf, dt, db)
+    _ -> Left $ "Expected function, but got " ++ show (brh ([], ft))
+  beq ([], at) (sf, dt)
+  pure $ br (sf, db)
 
 main :: IO ()
 main = putStr ""
